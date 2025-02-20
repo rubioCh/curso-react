2b7c8cc2c4b2fcea359efdd5ac3ab67898cab527
Carlos Rubio <107649363+rubioCh@users.noreply.github.com>
2025-02-20 16:13:32 -0600

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,53 @@
-import { useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import './App.css'
-import {Button} from "./components"
 
 function App() {
-  const [count, setCount] = useState(0)
-  /*const style = {
-    button: {
-      color: "blue",
-      backgroundColor: "grey"
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState("")
+
+  const fetchData =  async () => {
+    setLoading(true)
+      try {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+        if( !response.ok) {
+          throw new Error("Error al obtener datos")
+        }
+
+        const jsonData = await response.json()
+        setData(jsonData)
+      } catch (err) {
+        setError(err as string)
+      } finally {
+        setLoading(false)
+      }
     }
-  }*/
-  const countMore = () => {
-    setCount((count) => count + 1);
+  // comunicamos con un endpoint - entidad externa al componente
+  // oeraciones async
+  // parametros de entrada
+
+  //maneja el ciclo de vida de un componente
+  useEffect( () => {
+    fetchData()
+    // logica? que logica ? cuando se ejecuta esta logica?
+    // 1 - Cuando se monta el componente
+    // 2 - cada vez que se modifique uno de los valores del state
+    
+    // return () => {
+      // cuando se destruya el componente
+     //   manejar el estado de la memoria
+    //} 
+  },[])
+  if (loading) {
+    return <div>Cargando...</div>
   }
+
+  if (error) {
+    return <div>UPS! Hay un Error: {error} </div>
+  }
+
   return (
-    <>
-      <Button label={`Count is ${count}`} parentMethod={countMore}/> 
-    </>
+      <div>{JSON.stringify(data)}</div> 
   )
 }
 
